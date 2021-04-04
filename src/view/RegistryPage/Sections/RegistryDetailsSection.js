b7a38cb684ffd7bcba09dbeda6124a3e64a0c742
Mikel Millard <mikel.millard08@hotmail.com>
2021-04-04 13:07:31 -0400

    Updating registry page to link to the two additional registries that have been created.

--- src/view/RegistryPage/Sections/RegistryDetailsSection.js
+++ src/view/RegistryPage/Sections/RegistryDetailsSection.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import classNames from 'classnames';
 import travelersJoy from '../../../assets/img/travelers_joy.png';
+import macys from '../../../assets/img/macys.jpg';
+import bedBathBeyond from '../../../assets/img/bedBathBeyond.jpg';
 import styles from '../../../assets/jss/website-template/views/registryPageSections/RegistryDetails';
 import {makeStyles} from '@material-ui/core/styles';
 
@@ -23,11 +25,14 @@ export default function RegistryDetailsSection() {
 
                 <br/>
                 <h4>
-                    Below are the links to our current registries. We are currently registered at (insert places here),
-                    but we also have registered at a honeymoon registry. If you feel inclined to help with experiences
-                    on our honeymoon rather than physical gifts, feel free to check out the below link to Travelers Joy.
+                    Wedding gits are not expected from any of the wedding or secondary reception guests. However, below
+                    are the links to our current registries if you feel inclined. We are currently registered at Macy's
+                    and Bed Bath and Beyond,  but we also have registered at a honeymoon registry. If you feel inclined
+                    to help with experiences on our honeymoon rather than physical gifts, feel free to check out the
+                    below link to Travelers Joy.
                 </h4>
 
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <a
                     href="http://www.travelersjoy.com/mikellovesabigail/"
                     target="_blank"
@@ -35,6 +40,30 @@ export default function RegistryDetailsSection() {
                 >
                     <img
                         src={travelersJoy}
+                        alt="..."
+                        className={imageClasses}
+                    />
+                </a>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a
+                    href="https://www.macys.com/wgl/registry/guest/MIKEL-MILLARD-ABIGAIL-BROWN/7255839"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        src={macys}
+                        alt="..."
+                        className={imageClasses}
+                    />
+                </a>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a
+                    href="https://www.bedbathandbeyond.com/store/giftregistry/viewregistryguest/550148748?eventType=Wedding"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        src={bedBathBeyond}
                         alt="..."
                         className={imageClasses}
                     />
